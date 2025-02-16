cd3f5badb710516d9ffc74c059f723a2bc3522e2
Vinay Modepalli <vinaykumarmodepalli@gmail.com>
2025-02-16 19:06:20 +0530

    add simplejwt packages and update the requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'issues',
 
     'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}
